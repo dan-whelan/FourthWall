637c15097b8e873507fd35b43485a40e0f2f086b
Daniel Whelan <dpw0990@gmail.com>
2022-03-16 16:11:07 +0000

    added bare bones controller layout

--- FourthWall/FourthWall/Controllers/FourthWallController.cs
+++ FourthWall/FourthWall/Controllers/FourthWallController.cs
@@ -7,9 +7,52 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FourthWall.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
     public class FourthWallController : ControllerBase
     {
+        [HttpGet("/api/queries?from=&to=")] // May need to be changed
+        public int getQueries()
+        {
+            return 1;
+        }
+
+        [HttpGet("/api/transactions")]
+        public int getTransactions()
+        {
+            return 1;
+        }
+
+        [HttpGet("/api/statistics/{schema}/{table}")]
+        public int getTableStats()
+        {
+            return 1;
+        }
+
+        [HttpGet("/api/indexes/{schema}/{table}/{index}")]
+        public int getIndexUsage()
+        {
+            return 1;
+        }
+
+        [HttpGet("/api/indexes/{schema}/{table}")]
+        public int getAllIndexUsage()
+        {
+            return 1;
+        }
+
+        [HttpPost("/api/query")]
+        public int postExplainAnalyse()
+        {
+            //
+            // ASK: how to get input on query to post?
+            //
+            return 1;
+        }
+
+        [HttpGet("/api/data/{schema}/{table}")]
+        public int getSystemTableData()
+        {
+            return 1;
+        }
     }
 }
