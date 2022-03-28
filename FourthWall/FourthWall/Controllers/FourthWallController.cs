f8a401239b5390593af1246f3e5109822dc0824c
Daniel Whelan <dpw0990@gmail.com>
2022-03-28 19:08:03 +0100

    Added comments to API Controller

--- FourthWall/FourthWall/Controllers/FourthWallController.cs
+++ FourthWall/FourthWall/Controllers/FourthWallController.cs
@@ -6,21 +6,44 @@ using System.IO;
 using System.Runtime.Serialization.Json;
 using System.Text;
 using FourthWall.Models;
-
+/*|-------------------------------------------------------------------------------------------|
+ * FourthWall API Controller
+ * 
+ * Controller that enables API to make use of GET and POST requests from Monitoring System
+ *|-------------------------------------------------------------------------------------------|
+ */
 namespace FourthWall.Controllers
 {
     [ApiController]
     public class FourthWallController : ControllerBase
     {
+        //serializer that allows for the conversion of type List<Dictionary<string, string>> to JSON format
         private DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(List<Dictionary<string, string>>));
+
+        //serializer that allows for the conversion of type Dictionary<string, string> to JSON format
         private DataContractJsonSerializer dictSerializer = new DataContractJsonSerializer(typeof(Dictionary<string, string>));
+
+        /*|--------------------------------------------------------------------------------------------|
+         * getQueries()
+         * 
+         * GET request that takes a time period from header and returns any queries that have been 
+         * running for that length of time.
+         * 
+         * @params
+         * DateTime from: a time given by the user of when to start check of queries
+         * DateTime to:   a time given by the user of when to end check of queries
+         * 
+         * @return
+         * IActionResult: a HTTP Response to the HTTP request either Ok() containing JSON formatted file
+         * or NotFound()
+         *|--------------------------------------------------------------------------------------------|
+         */
         [HttpGet("/api/queries/{from:datetime?}/{to:datetime?}")]
         public IActionResult getQueriesAPI(DateTime from, DateTime to)
         {
             try
             {
-                List<Dictionary<string, string>> queries = FourthWall.Program
-                    .getLongRunningQueries();
+                List<Dictionary<string, string>> queries = FourthWall.Program.getLongRunningQueries();
                 if (queries != null)
                 {
                     using (MemoryStream ms = new MemoryStream())
@@ -39,6 +62,20 @@ namespace FourthWall.Controllers
             }
 
         }
+        /*|--------------------------------------------------------------------------------------------|
+         * getQueries()
+         * 
+         * GET request that returns JSON formatted result containing a list of all open transactions at 
+         * the time of request
+         * 
+         * @params
+         * Null
+         * 
+         * @return
+         * IActionResult: a HTTP Response to the HTTP request either Ok() containing JSON formatted file
+         * or NotFound()
+         *|--------------------------------------------------------------------------------------------|
+         */
 
         //[HttpGet("/api/transactions")]
         //public IActionResult getTransactions()
@@ -65,6 +102,21 @@ namespace FourthWall.Controllers
         //    }
         //}
 
+        /*|--------------------------------------------------------------------------------------------|
+         * getTableStats()
+         * 
+         * GET request that returns a JSON formatted list of all statistics related to a given schema 
+         * and table taken in from request header
+         * 
+         * @params
+         * string schema: a title of a schema in a given database
+         * string table:  a title of a table in the schema given prior
+         * 
+         * @return
+         * IActionResult: a HTTP Response to the HTTP request either Ok() containing JSON formatted file
+         * or NotFound()
+         *|--------------------------------------------------------------------------------------------|
+         */
         [HttpGet("/api/statistics/{schema}/{table}")]
         public IActionResult getTableStatsAPI(string schema, string table)
         {
@@ -90,6 +142,23 @@ namespace FourthWall.Controllers
 
         }
 
+
+        /*|--------------------------------------------------------------------------------------------|
+         * getIndexUsage()
+         * 
+         * GET request that returns a JSON formatted list of all usage of a specified index related to 
+         * a given schema and table taken in from request header
+         * 
+         * @params
+         * string schema: a title of a schema in a given database
+         * string table:  a title of a table in the schema given prior
+         * string index:  an index related to the table and schema given prior
+         * 
+         * @return
+         * IActionResult: a HTTP Response to the HTTP request either Ok() containing JSON formatted file
+         * or NotFound()
+         *|--------------------------------------------------------------------------------------------|
+         */
         [HttpGet("/api/indexes/{schema}/{table}/{index}")]
         public IActionResult getIndexUsageAPI(string schema, string table, string index)
         {
@@ -114,6 +183,22 @@ namespace FourthWall.Controllers
             }
         }
 
+
+        /*|--------------------------------------------------------------------------------------------|
+         * getAllIndexUsage()
+         * 
+         * GET request that Returns a JSON formatted list of all usage of all indices related to a 
+         * given schema and table taken in from request header
+         * 
+         * @params
+         * string schema: a title of a schema in a given database
+         * string table:  a title of a table in the schema given prior
+         * 
+         * @return
+         * IActionResult: a HTTP Response to the HTTP request either Ok() containing JSON formatted file
+         * or NotFound()
+         *|--------------------------------------------------------------------------------------------|
+         */
         [HttpGet("/api/indexes/{schema}/{table}")]
         public IActionResult getAllIndexUsageAPI(string schema, string table)
         {
@@ -138,6 +223,24 @@ namespace FourthWall.Controllers
             }
         }
 
+        /*|--------------------------------------------------------------------------------------------|
+         * postExplainAnalyse()
+         * 
+         * POST request that takes in a query in the body of the request and returns a JSON formatted 
+         * list that contains the result of the EXPLAIN ANALYZE
+         * 
+         * @params
+         * Query q: A PostGreSQL query taken in from the body of the request
+         * 
+         * e.g.
+         * Content-Type: application/json
+         * {"query: <query-text>;"}
+         * 
+         * @return
+         * IActionResult: a HTTP Response to the HTTP request either Ok() containing JSON formatted file
+         * or NotFound()
+         *|--------------------------------------------------------------------------------------------|
+         */
         [HttpPost("/api/query/")]
         public IActionResult postExplainAnalyseAPI([FromBody] Query q)
         {
@@ -162,6 +265,20 @@ namespace FourthWall.Controllers
             }
         }
 
+        /*|--------------------------------------------------------------------------------------------|
+         * getSystemTableData()
+         * 
+         * Returns a JSON formatted list of all data contained within a specified table
+         * 
+         * @params
+         * string schema: a title of a schema in a given database
+         * string table:  a title of a table in the schema given prior
+         * 
+         * @return
+         * IActionResult: a HTTP Response to the HTTP request either Ok() containing JSON formatted file
+         * or NotFound()
+         *|--------------------------------------------------------------------------------------------|
+         */
         [HttpGet("/api/data/{schema}/{table}")]
         public IActionResult getSystemTableDataAPI(string schema, string table)
         {
