a921fa415a2cbe1b01e3aec99777f7545346d79c
Daniel Whelan <dpw0990@gmail.com>
2022-04-09 13:37:01 +0100

    Added beginnings of Test Suite for API, contains tests for ok responses

--- FourthWall/FourthWall.Tests/APITest.cs
+++ FourthWall/FourthWall.Tests/APITest.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Web.Http.Results;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FourthWall.Controllers;
 using FourthWall.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FourthWall.Tests
 {
@@ -15,8 +15,42 @@ namespace FourthWall.Tests
         public void testGetTableStats()
         {
             var controller = new FourthWallController();
-            var result = controller.getTableStatsAPI("college", "student");
+            var result = controller.getTableStatsAPI("college", "student") as IActionResult;
+            Assert.IsInstanceOfType(result, typeof(OkResult)); 
+        }
+
+        [TestMethod]
+        public void testGetAllIndexUsage()
+        {
+            var controller = new FourthWallController();
+            var result = controller.getAllIndexUsageAPI("college", "student") as IActionResult;
             Assert.IsInstanceOfType(result, typeof(OkResult));
+        }
+
+        [TestMethod]
+        public void testSpecificIndexUsage()
+        {
+            var controller = new FourthWallController();
+            var result = controller.getIndexUsageAPI("college", "student", "idx_address_city") as IActionResult;
+            Assert.IsInstanceOfType(result, typeof(OkResult));
+        }
+
+        [TestMethod]
+        public void testGetTableData()
+        {
+            var controller = new FourthWallController();
+            var result = controller.getSystemTableDataAPI("college", "student") as IActionResult;
+            Assert.IsInstanceOfType(result, typeof(OkResult));
+        }
+
+        [TestMethod]
+        public void testPostExplainAnalyze()
+        {
+            var controller = new FourthWallController();
+            var query = new Query { query = "SELECT * FROM college.student" };
+            var result = controller.postExplainAnalyseAPI(query) as IActionResult;
+            Assert.IsInstanceOfType(result, typeof(OkResult));
+            
         }
     }
 }
